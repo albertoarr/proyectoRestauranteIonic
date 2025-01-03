34916d9c178a6d8e1ceba3641ce662d1e9b0ef9f
albertoarr <albertoarranzromano@gmail.com>
2025-01-03 22:03:50 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoRestauranteIonic',
+  webDir: 'dist'
+};
+
+export default config;
